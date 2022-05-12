01558f427460d5b392ed874eac60da3ecd23bfa0
matformation <game@le-scarabit.xyz>
2022-05-12 23:59:34 +0200

    feature: adding name choice for character

--- testtaper/src/main/java/org/example/Main.java
+++ testtaper/src/main/java/org/example/Main.java
@@ -1,15 +1,22 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Scanner;
+
 import static org.example.Battle.battle;
 
 
 public class Main {
     public static void main(String[] args) {
-        Hero player = new Hero("Ilgard", 600);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Good day to you, master. How shall I call you?");
+        //no limit à la longueur du nom. It's not a bug, it's a feature!
+        //Si ça devait ne plus être amusant (we are not amused), penser à limiter avec un if userName.length...
+        String userName = scanner.nextLine();
+        Hero player = new Hero(userName, 600);
         ArrayList<Monster> ennemies = Ennemies.Create(3);
-        System.out.println(player.getName());
         battle(player, ennemies);
 
     }
